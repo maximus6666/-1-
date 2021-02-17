c28ae9033c812565df8f6f5b1f5f043e5b043ac4
Maksym Dmytryshyn <dmytryshyn_max@ukr.net>
2021-02-17 20:20:20 +0200

    finish advanced part

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const cola = 15.678,
     fanta = 90.2345;
 
 //максимальне число
-const maxPrise = Math.max(cola, pepsi, fanta);
-console.log(`максимальне число ${maxPrise}`);
+const maxPrice = Math.max(cola, pepsi, fanta);
+console.log(`максимальне число ${maxPrice}`);
 
 
 //мінімальне число
-const minPrise = Math.min(cola, pepsi, fanta);
-console.log(`мінімальне число ${minPrise}`);
+const minPrice = Math.min(cola, pepsi, fanta);
+console.log(`мінімальне число ${minPrice}`);
 
 
 //вартість всіх товарів
@@ -49,24 +49,24 @@ const restOfMoney = 500 - sum;
 console.log(`сума решти при оплаті всіх товарів, клієнт платить 500: ${restOfMoney}`);
 
 //середнє значення цін, округлене до другого знаку після коми
-const roundedMiddlePrise = Number(((cola + pepsi + fanta) / 3).toFixed(2));
-console.log(`середнє значення цін, округлене до другого знаку після коми ${roundedMiddlePrise}`);
+const roundedMiddlePrice = Number(((cola + pepsi + fanta) / 3).toFixed(2));
+console.log(`середнє значення цін, округлене до другого знаку після коми ${roundedMiddlePrice}`);
 
 //Чистий прибуток 
 const randomSales = Math.ceil((Math.random() * 100));
-const priseWithSale = Number((sum - sum * randomSales / 100).toFixed(2));
-console.log(`ціна зі знижкою ${priseWithSale}`);
+const PriceWithSale = Number((sum - sum * randomSales / 100).toFixed(2));
+console.log(`ціна зі знижкою ${PriceWithSale}`);
 const profit = Number((sum / 2 - sum * randomSales / 100).toFixed(2));
 console.log(`прибуток при сумі ${sum.toFixed(2)} зі знижкою ${randomSales}% складає ${profit}`);
 
 //шаблонний рядок
-const totalString = `Максимальна ціна: ${maxPrise}
-Мінімальна ціна: ${minPrise}
+const totalString = `Максимальна ціна: ${maxPrice}
+Мінімальна ціна: ${minPrice}
 Вартість всіх товарів: ${sum}
 Cума без копійок: ${roundedSum}
 Cума товарів округлена до сотень ${roundToHundreds}
 Чи є сума всіх товарів парним числом: ${isEven}
 Cума решти при оплаті всіх товарів, клієнт платить 500: ${restOfMoney}
-Cереднє значення цін ${roundedMiddlePrise}`;
+Cереднє значення цін ${roundedMiddlePrice}`;
 
 console.log(totalString);
